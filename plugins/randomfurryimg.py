11f75db68bb73cc9a986bea8bcad8193c179070f
开普以尔 <100816732+kaixinol@users.noreply.github.com>
2022-07-30 14:44:39 +0800

    Update randomfurryimg.py

--- plugins/randomfurryimg.py
+++ plugins/randomfurryimg.py
@@ -4,7 +4,11 @@ import html2text
 import base64
 import json
 import random
+import re
+
 def GetFurryJson(Tag: str,config: dict):
+ if re.compile(u'[\u4e00-\u9fa5]').search(Tag):
+  return None
  base64string = base64.b64encode(bytes('%s:%s' % (config["username"], config["secret"]),'ascii'))
  url="https://e621.net/posts.json?tags=rating:s+"+Tag+"&limit="+str(config["limit"])
  print(url)
@@ -15,9 +19,9 @@ def GetFurryJson(Tag: str,config: dict):
  return json.loads(html.decode("utf-8"))
 
 def GetRandomFurryImg(config: dict,Tag: str):
- #try:
+ try:
   buffer=GetFurryJson(Tag,config)["posts"]
   aBuffer=buffer[random.randint(0,len(buffer)-1)]
   return {'url':aBuffer["sample"]["url"],'sources':aBuffer["sources"],'id': aBuffer["id"]}
- #except:
-  #return {'url':r"https://s1.ax1x.com/2022/07/23/jXR9D1.png",'sources':"None",'id': 114514}
+ except:
+  return {'url':r"https://s1.ax1x.com/2022/07/23/jXR9D1.png",'sources':"None",'id': 114514}
